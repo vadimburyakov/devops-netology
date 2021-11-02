6ec932628c679ca5918dcd0afa85f13dce0a0087
Vadim Buryakov <vadim_buryakov@mail.ru>
2021-11-02 20:11:09 +0300

    Merge branch 'git-merge'

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -3,7 +3,11 @@
 
 count=1
 for param in "$@"; do
+<<<<<<< HEAD
     echo "Next parameter: $param"
+=======
+    echo "Parameter: $param"
+>>>>>>> 90b6f77 (git rebase 1)
     count=$(( $count + 1 ))
 done
 
